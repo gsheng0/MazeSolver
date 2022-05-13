1395af7284c0dc0d48512aa464bba9d7bf28ce5d
George Sheng <gsheng112@gmail.com>
2022-05-13 16:12:41 -0400

    Added missing final keyword to constants

--- src/util/Constants.java
+++ src/util/Constants.java
@@ -8,11 +8,11 @@ public class Constants {
     public static final int MARGIN_SIZE = 100;
     public static final int NUM_HORIZONTAL_INTERSECTIONS = 10;
     public static final int NUM_VERTICAL_INTERSECTIONS = 7;
-    public static int CELL_WIDTH = (WINDOW_WIDTH - 2 * MARGIN_SIZE) / NUM_HORIZONTAL_INTERSECTIONS;
-    public static int CELL_HEIGHT = (WINDOW_HEIGHT - 2 * MARGIN_SIZE) / NUM_VERTICAL_INTERSECTIONS;
+    public static final int CELL_WIDTH = (WINDOW_WIDTH - 2 * MARGIN_SIZE) / NUM_HORIZONTAL_INTERSECTIONS;
+    public static final int CELL_HEIGHT = (WINDOW_HEIGHT - 2 * MARGIN_SIZE) / NUM_VERTICAL_INTERSECTIONS;
     public static final int SMALL_CIRCLE_HIT_BOX_DIAMETER = 40;
     public static final int SMALL_CIRCLE_RADIUS = 5;
-    public static int WALL_WIDTH = 2 * SMALL_CIRCLE_RADIUS + 2;
+    public static final int WALL_WIDTH = 2 * SMALL_CIRCLE_RADIUS + 2;
     public static final Point STARTING_POSITION = new Point(125, 125);
     public static final double VELOCITY_MAGNITUDE = 0.2;
     public static final int NUMBER_OF_BALLS = 15;
